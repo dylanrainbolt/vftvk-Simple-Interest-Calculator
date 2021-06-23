ee4ec41274dbb854ba6b32bfde66aba65d1438e5
dylanrainbolt <75927314+dylanrainbolt@users.noreply.github.com>
2021-06-23 08:41:03 -0700

    Show percentage rate right of slider.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function compute()
 {
-    var p = document.getElementById("principal").value;
-    var r= document.getElementById("rate").value;
-    var y= document.getElementById("years").value;
-    var i= p*(r/100)*y
-    var curr_y= new Date.getFullYear()
-    var year= y+curr_y
-    document.getElementById("result").innerHTML= "If you invest $"+ p+"\nAt a rate of "+r+"%\nYou will recieve an amount of $"+ parseFloat((i).toFixed(2)).toString()+" in year "+ year.toString()+".";
+    p = document.getElementById("principal").value;
+    r= document.getElementById("rate").value;
+    y= document.getElementById("years").value;
+    i= p*(r/100)*y
+    document.getElementById("result").innerHTML= parseFloat((i).toFixed(2)).toString();
 }
+var slider = document.getElementById("rate");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value+ "%";
+
+slider.oninput = function() {
+  output.innerHTML = this.value+"%";
+}
+        
