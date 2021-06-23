b8ebb90791c0a9e36487293c88dc6448007961a9
dylanrainbolt <75927314+dylanrainbolt@users.noreply.github.com>
2021-06-23 14:48:34 -0600

    Updated code to include slider value.

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-var slider = document.getElementById("rate");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value+ "%";
-
-slider.oninput = function() {
-  output.innerHTML = this.value+"%";
+function updateTextInput(val) {
+    document.getElementById('textInput').value=val; 
 }
 function compute()
 {
