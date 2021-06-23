be020fb5d0c6a5c27c57142a43fd754a461577b7
dylanrainbolt <75927314+dylanrainbolt@users.noreply.github.com>
2021-06-23 08:18:48 -0700

    Variable declaration

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    r= document.getElementById("rate").value;
-    y= document.getElementById("years").value;
-    i= p*(r/100)*y
-    curr_y= new Date.getFullYear()
-    year= y+curr_y
+    var p = document.getElementById("principal").value;
+    var r= document.getElementById("rate").value;
+    var y= document.getElementById("years").value;
+    var i= p*(r/100)*y
+    var curr_y= new Date.getFullYear()
+    var year= y+curr_y
     document.getElementById("result").innerHTML= "If you invest $"+ p+"\nAt a rate of "+r+"%\nYou will recieve an amount of $"+ parseFloat((i).toFixed(2)).toString()+" in year "+ year.toString()+".";
 }
