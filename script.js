021cf7cba65e6d7d75287114b8b567844a3f43e4
Dylan Rainbolt <dylanrainbolt@gmail.com>
2021-06-23 06:49:08 -0700

    ve changed js file so that interest is now caluclated and printed to screen

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r= document.getElementById("rate");
+    y= document.getElementById("years");
+    i= p*(r)*y
+    window.print(i)
 }
         
