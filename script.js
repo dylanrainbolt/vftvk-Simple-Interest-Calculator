527d53cf31430cf141a870e55c287dd84759c45b
dylanrainbolt <75927314+dylanrainbolt@users.noreply.github.com>
2021-06-23 08:13:11 -0700

    Changed js file to change inner html to print calculated values.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    r= document.getElementById("rate");
-    y= document.getElementById("years");
-    i= p*(r)*y
-    window.print(i)
+    r= document.getElementById("rate").value;
+    y= document.getElementById("years").value;
+    i= p*(r/100)*y
+    curr_y= new Date.getFullYear()
+    year= y+curr_y
+    document.getElementById("result").innerHTML= "If you invest $"+ p+"\nAt a rate of "+r+"%\nYou will recieve an amount of $"+ parseFloat((i).toFixed(2)).toString()+" in year "+ year.toString()+".";
 }
-        
