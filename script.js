64490bed6e8c350a2bbc0448af0e4fed9cd836de
dylanrainbolt <75927314+dylanrainbolt@users.noreply.github.com>
2021-06-24 16:02:20 -0700

    Initialized variables

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function updateTextInput(val) {
 }
 function compute()
 {
-    p = document.getElementById("principal").value;
-    r= document.getElementById("rate").value;
-    y= document.getElementById("years").value;
-    i= p*(r/100)*y
+    var p = document.getElementById("principal").value;
+    var r= document.getElementById("rate").value;
+    var y= document.getElementById("years").value;
+    var i= p*(r/100)*y
     document.getElementById("result").innerHTML= "If you invest $"+ p+"\nAt a rate of "+r+"%\nYou will recieve an amount of $"+ parseFloat((i).toFixed(2)).toString()+" in year "+ year.toString()+".";
 }
 
