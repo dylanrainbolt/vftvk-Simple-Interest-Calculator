9ca58abcdeb0ce3fbc69cbf6fa0659dabb9498f2
dylanrainbolt <75927314+dylanrainbolt@users.noreply.github.com>
2021-06-25 07:17:48 -0700

    Implemented new method of calling function onclick. Fixed error of calling wrong year variable.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function updateTextInput(val) {
     document.getElementById('textInput').value=val; 
 }
-function compute()
-{
-    var p = document.getElementById("principal").value;
-    var r= document.getElementById("rate").value;
-    var y= document.getElementById("years").value;
-    var i= p*(r/100)*y
-    document.getElementById("result").innerHTML= "If you invest $"+ p+"\nAt a rate of "+r+"%\nYou will recieve an amount of $"+ parseFloat((i).toFixed(2)).toString()+" in year "+ year.toString()+".";
-}
 
+document.getElementById("comp_button").onclick = function() {
+  var p = document.getElementById("principal").value;
+  //alert(p)
+  var r = document.getElementById("rate").value;
+  //alert(r)
+  var y = document.getElementById("years").value;
+  var i = p * (r / 100) * y
+  var message = "If you invest $" + p.toString() + "\nAt a rate of " + r.toString() + "%\nYou will recieve an amount of $" + i.toString() + " in year " + y.toString() + ".";
+  document.getElementById("result").innerHTML = message.toString();
+}
         
