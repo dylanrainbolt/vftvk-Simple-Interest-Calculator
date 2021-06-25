0c1c9fcc4b4caea01cdcf4a8d49f95f795b519d8
dylanrainbolt <75927314+dylanrainbolt@users.noreply.github.com>
2021-06-25 07:28:36 -0700

    Reformatted white space of lines 5-15

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function updateTextInput(val) {
 }
 
 document.getElementById("comp_button").onclick = function() {
-  var p = document.getElementById("principal").value;
-  alert(p)
-  var r = document.getElementById("rate").value;
-  alert(r)
-  var y = document.getElementById("years").value;
-  var i = p * (r / 100) * y
-  alert(i)
-  var message = "If you invest $" + p.toString() + "\nAt a rate of " + r.toString() + "%\nYou will recieve an amount of $" + i.toString() + " in year " + y.toString() + ".";
-  alert(message)
-  document.getElementById("result").innerHTML = message.toString();
+    var p = document.getElementById("principal").value;
+    alert(p)
+    var r = document.getElementById("rate").value;
+    alert(r)
+    var y = document.getElementById("years").value;
+    var i = p * (r / 100) * y
+    alert(i)
+    var message = "If you invest $" + p.toString() + "\nAt a rate of " + r.toString() + "%\nYou will recieve an amount of $" + i.toString() + " in year " + y.toString() + ".";
+    alert(message)
+    document.getElementById("result").innerHTML = message;
 }
         
