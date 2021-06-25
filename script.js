e14f3141894459d935517c7d23e2426e467d24f0
dylanrainbolt <75927314+dylanrainbolt@users.noreply.github.com>
2021-06-25 07:45:09 -0700

    went back to original onclick function method

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ function updateTextInput(val) {
     document.getElementById('textInput').value=val; 
 }
 
-document.getElementById("comp_button").onclick = function() {
-    var p = document.getElementById("principal").value;
-    alert(p)
-    var r = document.getElementById("rate").value;
-    alert(r)
-    var y = document.getElementById("years").value;
-    var i = p * (r / 100) * y
-    alert(i)
-    var message = "If you invest $" + p.toString() + "\nAt a rate of " + r.toString() + "%\nYou will recieve an amount of $" + i.toString() + " in year " + y.toString() + ".";
-    alert(message)
-    document.getElementById("result").innerHTML = message;
+function compute(){
+  var p = document.getElementById("principal").value;
+  //alert(p)
+  var r = document.getElementById("rate").value;
+  //alert(r)
+  var y = document.getElementById("years").value;
+  var i = p * (r / 100) * y
+  var message = "If you invest $" + p.toString() + "\nAt a rate of " + r.toString() + "%\nYou will recieve an amount of $" + i.toString() + " in year " + y.toString() + ".";
+  document.getElementById("result").innerHTML = message.toString();
 }
         
