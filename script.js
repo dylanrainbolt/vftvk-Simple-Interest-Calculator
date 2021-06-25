bbef186d3d829a617a5b89154156edb14e43f402
dylanrainbolt <75927314+dylanrainbolt@users.noreply.github.com>
2021-06-25 08:03:41 -0700

    Created if else statement so user inputs num for principal

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ function compute(){
   if (p <= 0){
       alert("Value must be a positive number.")
   }
-  //alert(p)
-  var r = document.getElementById("rate").value;
-  //alert(r)
-  var y = document.getElementById("years").value;
-  var i = p * (r / 100) * y
-  var message = "If you invest $" + p.toString() + "\nAt a rate of " + r.toString() + "%\nYou will recieve an amount of $" + i.toString() + " in year " + y.toString() + ".";
-  document.getElementById("result").innerHTML = message.toString();
+  else {
+      //alert(p)
+      var r = document.getElementById("rate").value;
+      //alert(r)
+      var y = document.getElementById("years").value;
+      var i = p * (r / 100) * y
+      var message = "If you invest $" + p.toString() + "\nAt a rate of " + r.toString() + "%\nYou will recieve an amount of $" + i.toString() + " in year " + y.toString() + ".";
+      document.getElementById("result").innerHTML = message.toString();
+  }
 }
         
